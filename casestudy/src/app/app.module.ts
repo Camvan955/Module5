17aeafaa5bf472b14d115e9c47c55ec4bdf46db3
Camvan955 <nguyenvann955@gmail.com>
2022-12-24 17:08:40 +0700

    nop bai

--- casestudy/src/app/app.module.ts
+++ casestudy/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { HeaderComponent } from './component/layout/header/header.component';
+import { NavigationComponent } from './component/layout/navigation/navigation.component';
+import { FooterComponent } from './component/layout/footer/footer.component';
+import { ListComponent } from './component/facility/list/list.component';
+import { EditComponent } from './component/facility/edit/edit.component';
+import { CreateComponent } from './component/facility/create/create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    NavigationComponent,
+    FooterComponent,
+    ListComponent,
+    EditComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
