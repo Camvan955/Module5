17103a8fa480e3aaf3bb338b6f1e8e67749a8cac
Camvan955 <nguyenvann955@gmail.com>
2022-12-29 21:13:05 +0700

    nop bai

--- casestudy/src/app/contract/create-contract/create-contract.component.ts
+++ casestudy/src/app/contract/create-contract/create-contract.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-create-contract',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-contract.component.css']
 })
 export class CreateContractComponent implements OnInit {
+  formContractCreate: FormGroup;
 
-  constructor() { }
+  constructor() {
+    this.formContractCreate = new FormGroup({
+      id: new FormControl('', [Validators.required, Validators.pattern('HĐ-[0-9]{4}')]),
+      dateStart: new FormControl('', [Validators.required]),
+      dateEnd: new FormControl('', [Validators.required]),
+      deposit: new FormControl('', [Validators.required]),
+      totalPay: new FormControl('', [Validators.required]),
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  saveContract(){
+    console.log(this.formContractCreate.value);
+  }
 }
--- casestudy/src/app/facility/create-facility/create-facility.component.ts
+++ casestudy/src/app/facility/create-facility/create-facility.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {RenType} from '../ren-type';
 
 @Component({
   selector: 'app-create-facility',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-facility.component.css']
 })
 export class CreateFacilityComponent implements OnInit {
-
-  constructor() { }
+  createFacilityForm :FormGroup;
+  renTypeList= [
+    {id: 1, name: "theo ngày"},
+    {id: 2, name: "theo tuần"},
+    {id: 3, name: "theo tháng"},
+    {id: 4, name: "theo giờ"},
+  ]
+  constructor() {
+    this.createFacilityForm = new FormGroup({
+      id: new FormControl('', [Validators.required, Validators.pattern('DV-[0-9]{4}')]),
+      name: new FormControl('',[Validators.required,Validators.pattern("([A-Z][a-z]+[ ])+([A-Z][a-z]+)")]),
+      area: new FormControl('', [Validators.required, Validators.min(0)]),
+      numberOfFloor: new FormControl('', [Validators.required]),
+      maxPeople: new FormControl('', [Validators.required]),
+      cost: new FormControl('', [Validators.required]),
+      poolArea: new FormControl('', [Validators.required]),
+      renType: new FormControl('', [Validators.required]),
+      standardRoom: new FormControl('', [Validators.required]),
+    })
+  }
 
   ngOnInit(): void {
+  }
+  saveFacility(){
+    console.log(this.createFacilityForm.value);
   }
 
 }
